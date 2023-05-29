9cdd903eabc6627e4acd84befdb5b503dacf7e25
Asad Abbas <asadmajid420@gmail.com>
2023-05-29 04:30:15 -0700

    server inititlez usrer and userandBlog sacheam completed

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express")
-
+const mongoose = require('mongoose')
 
 const app  = express()
 
 app.listen(5000,()=>{
     console.log("server as listing to 5000")
     
+})
+mongoose.connect('mongodb://127.0.0.1/bloging_corner').then(()=>{
+    console.log('database conect succefully')
+}).catch(err=>{
+    console.log(err)
 })
