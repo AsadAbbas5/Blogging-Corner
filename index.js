b0b3a9936c307fd31c6b5fb50020009a80dd90bb
Asad Abbas <asadmajid420@gmail.com>
2023-06-20 04:40:29 -0700

    Server initializing

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const express = require("express")
 const mongoose = require('mongoose')
 
@@ -12,3 +13,12 @@ mongoose.connect('mongodb://127.0.0.1/bloging_corner').then(()=>{
 }).catch(err=>{
     console.log(err)
 })
+=======
+const express = require('express')
+
+const app = express()
+
+app.listen(5000,()=>{
+    console.log('app listen to at 5000')
+})
+>>>>>>> Stashed changes
